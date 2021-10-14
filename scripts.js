3954457d30e488d3a2fd32d4eb95906f6345a3c7
ezzy-web <90319401+ezzy-web@users.noreply.github.com>
2021-10-14 14:30:12 -0500

    JavaScript Completed

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener('DOMContentLoaded', () => {
+    const email = document.getElementById('email')
+    const form = Array.from(document.getElementsByTagName('form'))[0]
+    const msg = Array.from(document.getElementsByClassName('message'))[0]
+    console.log(form, msg)
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        if (email.value === '') {
+            msg.innerHTML = "<p>Please enter a valid email address</p>"
+        } else {
+            msg.innerHTML = "<p>Thank you! Your email address " + email.value + "</p>"
+        }
+    })
+})
